0e7b0cb9e935634aa8aa25a3e794f0f49f613719
Ali-Amin29 <ali.amin2910@gmail.com>
2023-02-22 13:27:05 +0200

    Adding LiveWire And Add Task with priority Input

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('task.index')->middleware('auth');
+Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store'])->name('task.store');
